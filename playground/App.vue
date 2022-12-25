6c1a38522edc0a5811ccf105a923f9456a345790
Sadegh Barati <sadeghbaratiwork@gmail.com>
2022-12-25 19:28:39 +0330

    refactor: restructer dev and rename to playground

---
+++ playground/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import DatatablesnetVue from '../src';
+import Test from './Test.vue';
+import DataTablesLib from 'datatables.net';
+
+const data = [
+  [1, 2],
+  [3, 4],
+];
+
+if(typeof window !== 'undefined') {
+  DatatablesnetVue.use(DataTablesLib)
+}
+</script>
+
+<template>
+  <div>
+
+    <Test />
+
+    <div class="m-48">
+      <DatatablesnetVue :data="data" class="display">
+        <thead>
+            <tr>
+                <th>A</th>
+                <th>B</th>
+            </tr>
+        </thead>
+      </DatatablesnetVue>
+    </div>
+  </div>
+</template>
+
+<style>
+  .m-48 {
+    margin: 3rem
+  }
+</style>

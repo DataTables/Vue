c5307598f73111a78b6db349dd64a0f905d00574
Allan Jardine <allan@datatables.net>
2023-10-06 14:58:36 +0100

    New: Support for column templates, providing the ability to use Vue components in DataTables' columns. This new ability requires DataTables 2 (still in development at the time of this commit)
    Use a example with the slot name `column-X` where `X` is the column
    index that you want the template to apply to. DataTables will render the
    template via Vue's methods, and insert it into the table for each cell
    in that column. This is implemented by using a rendering function for
    the column. DataTables 2's ability to have an HTML element returned
    from the rendering function is key here.

--- src/datatables.net-vue.vue
+++ src/datatables.net-vue.vue
@@ -1,18 +1,27 @@
 <script lang="ts">
-  export let DataTablesLib: any
+export let DataTablesLib: any;
 
-  export default {
-    name: 'Datatables.netVue',
-    inheritAttrs: false,
-    use(lib: any) {
-      DataTablesLib = lib;
-    }
-  }
+export default {
+	name: 'Datatables.netVue',
+	inheritAttrs: false,
+	use(lib: any) {
+		DataTablesLib = lib;
+	}
+};
 </script>
 
 <script lang="ts" setup>
 // imports
-import { ref, unref, watch, onMounted, onBeforeUnmount, getCurrentInstance} from 'vue';
+import {
+	h,
+	ref,
+	render,
+	unref,
+	watch,
+	onMounted,
+	onBeforeUnmount,
+	getCurrentInstance
+} from 'vue';
 import type { Api, Config, ConfigColumns } from 'datatables.net';
 import dtEvents from './dtEvents';
 
@@ -41,6 +50,7 @@ defineEmits(dtEvents);
 
 // setup
 const table = ref<HTMLTableElement | null>(null);
+const elements: { [key: string]: HTMLElement } = {};
 
 // data
 const dt = ref<Api<any>>();
@@ -52,31 +62,23 @@ const oldData = ref<any[]>([]);
 watch(
 	() => props.data,
 	(newVal) => {
-		let known = dt.value?.data().toArray();
+		let api = dt.value;
 
-		// Find any new rows
-		for (let n of newVal) {
-			if (!known?.includes(n)) {
-				dt.value?.row.add(n);
-			}
+		if (!api) {
+			return;
 		}
 
-		// Remove any old rows
-		if (typeof known !== 'undefined') {
-			for (let k of known) {
-				if (!newVal.includes(k)) {
-					dt.value?.row((_idx: any, d: any) => d === k).remove();
-				}
-			}
+		var keys = Object.keys(elements);
+
+		for (var i = 0; i < keys.length; i++) {
+			delete elements[keys[i]];
 		}
 
-		// Data in other rows might have changes, so we need to invalidate the rows
-		dt.value?.rows().invalidate().draw(false);
-
-		saveOld(newVal);
+		api.clear();
+		api.rows.add(newVal).draw(false);
 	},
 	{
-		deep: true,
+		deep: true
 	}
 );
 
@@ -104,6 +106,55 @@ onMounted(() => {
 		);
 	}
 
+	if (!options.columnDefs) {
+		options.columnDefs = [];
+	}
+
+	if (inst) {
+		let slotNames = Object.keys(inst.slots);
+
+		if (slotNames.length && !DataTablesLib.versionCheck('2')) {
+			throw new Error(
+				'DataTables 2 is required to be able to use template slots for columns.'
+			);
+		}
+
+		slotNames.forEach((name) => {
+			if (name.match(/^column-\d+$/)) {
+				options.columnDefs.push({
+					target: parseInt(name.replace('column-', '')),
+					render: function (
+						data: any,
+						type: string,
+						row: any,
+						meta: any
+					) {
+						let key = meta.row + ',' + meta.col;
+
+						if (!elements[key]) {
+							elements[key] = document.createElement('div');
+
+							let content = h(
+								'div',
+								inst!.slots[name]!({
+									cellData: data,
+									colIndex: meta.col,
+									rowData: row,
+									rowIndex: meta.row,
+									type
+								})
+							);
+
+							render(content, elements[key]);
+						}
+
+						return elements[key].childNodes[0];
+					}
+				});
+			}
+		});
+	}
+
 	// Create the DataTable!
 	dt.value = new DataTablesLib(unref(table), options);
 
@@ -111,7 +162,7 @@ onMounted(() => {
 	// `on` method and then emit them to our Vue component
 	for (let eName of dtEvents) {
 		if (dt.value && inst) {
-			dt.value.on(eName, function() {
+			dt.value.on(eName, function () {
 				var args = Array.from(arguments);
 				var event = args.shift();
 
@@ -138,15 +189,14 @@ defineExpose({
 	/**
 	 * DataTable instance
 	 */
-	dt,
+	dt
 });
-
 </script>
 
 <template>
-  <div class="datatable" v-once>
-    <table ref="table" v-bind="$attrs" style="width:100%">
-      <slot></slot>
-    </table>
-  </div>
+	<div class="datatable" v-once>
+		<table ref="table" v-bind="$attrs" style="width: 100%">
+			<slot></slot>
+		</table>
+	</div>
 </template>
